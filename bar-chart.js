18b342f44e323c1e34df17189a7ce0a8b8af0679
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-03 14:57:39 -0800

    Starting to set up connection between html>js

---
+++ bar-chart.js
@@ -0,0 +1,12 @@
+let drawBarChart = function(data, options, element){
+  let barsSize;//The data parameter will be the data the chart should work from Start with just an Array of numbers
+  // e.g. [1, 2, 3, 4, 5]
+  let optionsGiven;//{ options argument goes here
+    // width: 0,
+    // height:0,
+    // The options parameter should be an object which has options for the chart.
+  // e.g. width and height of the bar chart
+   //The element parameter should be a DOM element or jQuery element that the chart will get rendered into.
+   let domElement = document.getElementById("barchart")
+  }
+
